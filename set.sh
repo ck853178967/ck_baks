309156c8faf147efa9097e39b649ab5da04dc4e6
ck853178967 <853178967@qq.com>
2024-09-14 17:54:43 +0800

    Create set.sh

---
+++ set.sh
@@ -0,0 +1,11 @@
+#!/bin/bash                                                                                                                                               
+ 
+cp -r .vim ~/
+cp .vimrc ~/
+cp .tmux.conf ~/
+ 
+echo "alias ll='ls -l -a'" >> ~/.bashrc
+echo "alias tmux='TERM=xterm-256color tmux'" >> ~/.bashrc
+echo "PS1='\[\033[33;1m\][\[\e[35m\]\T \[\033[33;1m\]\u\[\033[34m\]@\h:\[\e[0m\]\[\033[32;1m\]\W\[\033[33m\]]\$ \[\033[0m\]'" >> ~/.bashrc
+
+# source ~/.bashrc
